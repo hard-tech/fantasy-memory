1421a665d9053584133b3b94b1194122c427f65b
Alexandre Perché <a.perche.dev@gmail.com>
2023-10-23 14:17:43 +0200

    Added the database init sql file

---
+++ assets/sql/init.sql
@@ -0,0 +1,56 @@
+/* DATABASE AND TABLES */
+
+DROP DATABASE IF EXISTS fantasy_memory;
+CREATE DATABASE fantasy_memory;
+USE fantasy_memory;
+
+CREATE TABLE game (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(32)
+)
+CHARACTER SET "utf8"
+ENGINE=INNODB;
+
+CREATE TABLE player (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(256) NOT NULL,
+    pwd VARCHAR(256) NOT NULL,
+    sign_up_timestamp DATE NOT NULL,
+    latest_connection_timestamp DATE NOT NULL   
+)
+CHARACTER SET "utf8"
+ENGINE=INNODB;
+
+CREATE TABLE score (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    player_id INT UNSIGNED NOT NULL,
+    game_id INT UNSIGNED NOT NULL,
+	game_difficulty ENUM("easy", "medium", "hard") NOT NULL,
+    score INT UNSIGNED NOT NULL,
+    score_timestamp DATE NOT NULL
+)
+CHARACTER SET "utf8"
+ENGINE=INNODB;
+
+CREATE TABLE message (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    game_id INT UNSIGNED NOT NULL,
+    player_id INT UNSIGNED NOT NULL,
+    message TEXT NOT NULL,
+    message_timestamp DATE NOT NULL
+)
+CHARACTER SET "utf8"
+ENGINE=INNODB;
+
+/* FOREIGN KEYS */
+ALTER TABLE score
+ADD CONSTRAINT fk_score_player FOREIGN KEY (player_id) REFERENCES player(id) ON DELETE CASCADE;
+
+ALTER TABLE score
+ADD CONSTRAINT fk_score_game FOREIGN KEY (game_id) REFERENCES game(id) ON DELETE CASCADE;
+
+ALTER TABLE message
+ADD CONSTRAINT fk_message_player FOREIGN KEY (player_id) REFERENCES player(id) ON DELETE CASCADE;
+
+ALTER TABLE message
+ADD CONSTRAINT fk_message_game FOREIGN KEY (game_id) REFERENCES game(id) ON DELETE CASCADE;
