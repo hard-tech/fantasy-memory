fc47f9f9a1994b7f764451dd6ae9646602125fb2
Alexandre Perché <a.perche.dev@gmail.com>
2023-10-31 15:32:26 +0100

    Form WIP

--- login.php
+++ login.php
@@ -18,12 +18,38 @@ $page = "login";
         </section>
         <section class="container justify-content-center">
             <form class="form-std">
-                <input type="text" placeholder="Email" ></input>
-                <input type="text" placeholder="Password" ></input>
+                <input type="text" placeholder="Email" name="email"></input>
+                <input type="text" placeholder="Password" name="pwd"></input>
                 <div>
                     <button type="submit">Log in</button>
                 </div>
             </form>
+            <?php 
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $name = $_POST["email"];
+                    $pwd = $_POST["pwd"];
+                    $pdoStatement = $pdo->prepare("
+                        SELECT p.id, p.pseudo FROM players AS p
+                        WHERE p.pseudo = $name AND p.pwd = $pwd;
+                    ");
+                    $pdoStatement->execute();
+                    $user = $pdoStatement->fetch();
+                    var_dump($user);
+                    if ($user) {
+                        $logInStatus = $pdo->prepare("
+                            UPDATE players AS p
+                            SET latest_connection_timestamp=NOW();
+                        ");
+                        if ($logInStatus->execute()) {
+                            $_SESSION["user"] = [
+                                "id" => $user->id,
+                                "pseudo" => $user->pseudo
+                            ];
+                            header("Location: localhost:8888/fantasy-memory/index.html");
+                        }
+                    }
+                }
+            ?>
         </section>
     </main>
 
